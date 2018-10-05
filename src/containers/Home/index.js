15247ee83fb47b080139a3dded7610d1c1fb49e0
Lac <laclys@126.com>
2018-10-06 00:11:24 +0800

    update script: use npm-run-all

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,12 @@
  * @Author: Lac 
  * @Date: 2018-10-05 22:26:38 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-10-06 00:02:21
+ * @Last Modified time: 2018-10-06 00:10:48
  */
 import React from 'react'
 
 const Home = () => {
-  return <div>home1</div>
+  return <div>home</div>
 }
 
 export default Home
