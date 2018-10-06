87cad5d6a5798ffb4d5bab6170b21005aada8b92
Lac <laclys@126.com>
2018-10-06 20:12:46 +0800

    merge webpack base config

--- webpack.client.js
+++ webpack.client.js
@@ -2,29 +2,19 @@
  * @Author: Lac 
  * @Date: 2018-10-06 13:30:49 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-10-06 13:34:41
+ * @Last Modified time: 2018-10-06 20:09:07
  */
 const path = require('path')
+const merge = require('webpack-merge')
+const config = require('./webpack.base')
 
-module.exports = {
+const clientConfig = {
   mode: 'development',
   entry: './src/client/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'public')
-  },
-  module: {
-    rules: [{
-      test: /\.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      options: {
-        presets: ['react', 'stage-0',['env', {
-          targets: {
-            browsers: ['last 2 versions']
-          }
-        }]]
-      }
-    }]
   }
 }
+
+module.exports = merge(config, clientConfig)
--- webpack.server.js
+++ webpack.server.js
@@ -1,13 +1,15 @@
 /*
  * @Author: Lac 
  * @Date: 2018-10-05 22:26:33 
- * @Last Modified by:   Lac 
- * @Last Modified time: 2018-10-05 22:26:33 
+ * @Last Modified by: Lac
+ * @Last Modified time: 2018-10-06 20:09:58
  */
 const path = require('path')
 const nodeExternals = require('webpack-node-externals')
+const merge = require('webpack-merge')
+const config = require('./webpack.base')
 
-module.exports = {
+const serverConfig = {
   target: 'node',
   mode: 'development',
   entry: './src/index.js',
@@ -15,19 +17,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
   },
-  externals: [nodeExternals()],
-  module: {
-    rules: [{
-      test: /\.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      options: {
-        presets: ['react', 'stage-0',['env', {
-          targets: {
-            browsers: ['last 2 versions']
-          }
-        }]]
-      }
-    }]
-  }
+  externals: [nodeExternals()]
 }
+
+module.exports = merge(config, serverConfig)
