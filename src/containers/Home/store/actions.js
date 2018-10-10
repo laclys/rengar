1411f3ee2ace9c90708e280cc8dade2a762bdde0
Lac <laclys@126.com>
2018-10-10 22:00:14 +0800

    use thunk.withExtraArgument

--- src/containers/Home/store/actions.js
+++ src/containers/Home/store/actions.js
@@ -2,25 +2,22 @@
  * @Author: Lac 
  * @Date: 2018-10-07 15:08:40 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-10-09 23:47:37
+ * @Last Modified time: 2018-10-10 21:57:58
  */
 import axios from 'axios'
 
 import { CHANGE_LIST } from './constants'
-import clientAxios from '../../../client/request'
-import serverAxios from '../../../server/request'
 
 const changeList = (list) => ({
   type: CHANGE_LIST,
   list
 })
 
-export const getHomeList = (server) => {
+export const getHomeList = () => {
   // 浏览器运行 /api/news.json = http://loacalhost:300/api/news.json
   // 服务器运行  /api/news.json = 服务器目录下的/api/news.json
-  let request = server ? serverAxios : clientAxios
-  return (dispatch) => {
-    return request.get('/api/news.json?secret=D37msjPeC3')
+  return (dispatch, getState, axiosInstance) => {
+    return axiosInstance.get('/api/news.json?secret=D37msjPeC3')
       .then(res => {
         const list = res.data.data
         dispatch(changeList(list))
--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,24 @@
  * @Author: Lac 
  * @Date: 2018-10-07 00:51:19 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-10-08 23:39:37
+ * @Last Modified time: 2018-10-10 21:55:43
  */
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { reducer as homeReducer } from '../containers/Home/store'
+import clientAxios from '../client/request'
+import serverAxios from '../server/request'
 
 const reducer = combineReducers({
   home: homeReducer
 })
 
 export const getStore = () => {
-  return createStore(reducer, applyMiddleware(thunk))
+  // 改变服务器端的store，需要用serverAxios
+  return createStore(reducer, applyMiddleware(thunk.withExtraArgument(serverAxios)))
 }
 
 export const getClientStore = () => {
   const defaultState = window.context.state
-  return createStore(reducer, defaultState, applyMiddleware(thunk))
+  return createStore(reducer, defaultState, applyMiddleware(thunk.withExtraArgument(clientAxios)))
 }
