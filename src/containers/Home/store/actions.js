6bb789bdd35a6506ac3a1cbaf380b0f587b3c5a4
Lac <laclys@126.com>
2018-10-13 21:36:31 +0800

    use yarn && fetch handle add catch

--- src/containers/Home/store/actions.js
+++ src/containers/Home/store/actions.js
@@ -2,7 +2,7 @@
  * @Author: Lac 
  * @Date: 2018-10-07 15:08:40 
  * @Last Modified by: Lac
- * @Last Modified time: 2018-10-10 21:57:58
+ * @Last Modified time: 2018-10-13 21:31:42
  */
 import axios from 'axios'
 
@@ -17,9 +17,12 @@ export const getHomeList = () => {
   // 浏览器运行 /api/news.json = http://loacalhost:300/api/news.json
   // 服务器运行  /api/news.json = 服务器目录下的/api/news.json
   return (dispatch, getState, axiosInstance) => {
-    return axiosInstance.get('/api/news.json?secret=D37msjPeC3')
+    return axiosInstance.get('/api/news.json?secret=M5s2sPneDE')
       .then(res => {
         const list = res.data.data
+        dispatch(changeList(list))
+      }).catch(err => {
+        const list = []
         dispatch(changeList(list))
       })
   }
